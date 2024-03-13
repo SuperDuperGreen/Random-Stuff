79b7dd40cb487db14188660b5efd7136543075b3
Caleb Brewer <118789120+SuperDuperGreen@users.noreply.github.com>
2024-03-13 14:56:45 -0700

    Begun the first phase

---
+++ you/creatingYOU.py
@@ -0,0 +1,20 @@
+import time
+import os
+
+textSpeed = 1
+
+print("Welcome")
+time.sleep(textSpeed)
+os.system('cls')
+print("YOU are about to go on a wonderful journey")
+time.sleep(textSpeed)
+os.system('cls')
+print("But first,")
+time.sleep(textSpeed)
+name = input("who are YOU?").upper()
+time.sleep(textSpeed)
+os.system('cls')
+print("What a lovely name.")
+time.sleep(textSpeed)
+os.system('cls')
+weapon = input("Now, what WEAPON would YOU use?").upper()
