4209de348576c0232e16c535fb9933612daeefe2
Caleb Brewer <118789120+SuperDuperGreen@users.noreply.github.com>
2024-03-14 20:21:38 -0700

    Started character builder

--- you/creatingYOU.py
+++ you/creatingYOU.py
@@ -1,20 +1,32 @@
 import time
 import os
 
-textSpeed = 1
+textSpeed = 2.5
+strong = 0
+tough = 0
+heart = 0
+quick = 0
+smart = 0
 
+os.system('cls')
 print("Welcome")
 time.sleep(textSpeed)
 os.system('cls')
 print("YOU are about to go on a wonderful journey")
 time.sleep(textSpeed)
 os.system('cls')
-print("But first,")
+print("But first")
 time.sleep(textSpeed)
-name = input("who are YOU?").upper()
+name = input("Who are YOU?\n").upper()
 time.sleep(textSpeed)
 os.system('cls')
 print("What a lovely name.")
 time.sleep(textSpeed)
 os.system('cls')
-weapon = input("Now, what WEAPON would YOU use?").upper()
+weapon = input("Now, what WEAPON would YOU use?\n").upper()
+print("That sounds fitting for YOU.")
+time.sleep(textSpeed)
+os.system('cls')
+print("Let us now decide your abilities. There are 5:\n")
+time.sleep(textSpeed)
+print("STRONG\nTOUGH\nHEART\nQUICK\nSMART")
